58ecba664fbeced7cb925d3353006e5eac834db7
Seulki Yun <icstuckyi@gmail.com>
2022-10-24 21:23:19 +0900

    [level 0] Title: 각도기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -0,0 +1,8 @@
+function solution(angle) {
+    switch(true){
+        case (angle>0 && angle<90): return 1;
+        case (angle==90): return 2;
+        case (angle>90 && angle<180): return 3;
+        case (angle==180): return 4;
+    }
+}
