163c49c830e123e9d6a15adf4952652e8ecf91c1
Seulki Yun <icstuckyi@gmail.com>
2022-11-14 21:07:27 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -0,0 +1,9 @@
+function solution(s1, s2) {
+    let arr = 0;
+    for(let i=0; i<s1.length; i++){
+        for(let j=0; j<s2.length; j++){
+            if(s1[i] === s2[j]) arr++;
+        }
+    }
+    return arr;
+}
